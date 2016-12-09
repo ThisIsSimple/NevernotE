81064fcb136e604ece7f3df4d90b17adfaee821f
dewberry9 <yjh00511@naver.com>
2016-12-10 01:34:55 +0900

    add electron app

--- electron-app/app/main.js
+++ electron-app/app/main.js
@@ -11,17 +11,21 @@ function createWindow(){
         height: 700,
         frame: false,
         //일렉트론 최초작동과 html 로드까지는 약간의 딜레이가 있다. backgroundColor는 최대한 네이티브 앱처럼 보이게 할 것이다
-        backgroundColor: '#e0e0e0',
+        backgroundColor: '#ffffff',
         //동시에 풀스크린을 명시적으로 false하므로써 MacOS에서의 전체화면 보기를 가린다
         fullscreen: false,
-        icon: 'C:/Users/My/NevernotE/electron-app/app/NavernotEsimple.ico'
+        icon: 'D:/NevernotE-test/NavernotEsimple.ico',
+        //show: false
     })
+
     //그리고 html 로드
-    win.loadURL(`file://${__dirname}/index.html`)
+    win.loadURL(`file://${__dirname}/mainpage.html`)
     //창이 닫힐 때, win의 값을 제.거.한다
     win.on('closed', function(){
         win = null
     })
+    //win.webContents.openDevTools()
+    
 }
 //MacOS 등의 OS에서 프로그램이 독 등에 남아 있을 경우, 재실행을 준비한다
 app.on('ready', createWindow)
